7a9d0618126e7f83a2d1ab35479edbea8f27e383
Ryan Kelly <ryan@ryankelly.us>
2019-04-25 10:04:25 -0400

    v1.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="spindrift",
-    version="1.3",
+    version="1.4",
     description="package python applications for AWS Lambda and AWS Elastic Beanstalk",
     author="Ryan P. Kelly",
     author_email="ryan@ryankelly.us",
