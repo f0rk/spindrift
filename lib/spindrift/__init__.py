f94cff5367fdeea60cde7783bd75781246c2ee72
Ryan Kelly <ryan@ryankelly.us>
2025-10-30 13:10:20 +0000

    we're at 5.7

--- lib/spindrift/__init__.py
+++ lib/spindrift/__init__.py
@@ -1,3 +1,3 @@
 # Copyright 2017-2025, Ryan P. Kelly.
 
-__version__ = "5.8"
+__version__ = "5.7"
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 
 setup(
     name="spindrift",
-    version="5.8",
+    version="5.7",
     description="package python applications for AWS Lambda, AWS Elastic Beanstalk, AWS Batch (Docker)",
     author="Ryan P. Kelly",
     author_email="ryan@ryankelly.us",
